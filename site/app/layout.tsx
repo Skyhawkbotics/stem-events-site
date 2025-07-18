c7969fbd04221d8dbd51cd6d2d88df72086c5743
Dev Sahay <59460385+Synaptic149@users.noreply.github.com>
2025-07-18 00:01:22 -0700

    remove some nextjs/supabase branding

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "STEM Events",
+  description: "The new way to find STEM events in your area",
 };
 
 const oxaniumSans = Oxanium({
