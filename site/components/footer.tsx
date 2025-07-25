ac4f21441968c22fe5688e528a81ab1cd2b3c388
Synaptic <devkrishnasahay@gmail.com>
2025-07-24 20:39:49 -0700

    fix indents in navbar and footer

--- site/components/footer.tsx
+++ site/components/footer.tsx
@@ -1,20 +1,20 @@
 import { ThemeSwitcher } from "@/components/theme-switcher";
 
 export function Footer() {
-  return (
-    <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-      <p>
-        Maintained by{" "}
-        <a
-          href="https://www.hawkbot1cs.org"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Hawkbot1cs
-        </a>
-      </p>
-      <ThemeSwitcher />
-    </footer>
-  );
+	return (
+		<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+		<p>
+			Maintained by{" "}
+			<a
+			href="https://www.hawkbot1cs.org"
+			target="_blank"
+			className="font-bold hover:underline"
+			rel="noreferrer"
+			>
+			Hawkbot1cs
+			</a>
+		</p>
+		<ThemeSwitcher />
+		</footer>
+	);
 } 
--- site/components/navbar.tsx
+++ site/components/navbar.tsx
@@ -4,14 +4,14 @@ import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
 export function Navbar() {
-  return (
-    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-      <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-        <div className="flex gap-5 items-center font-semibold">
-          <Link href={"/"}>STEM Events</Link>
-        </div>
-        {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                <div className="flex gap-5 items-center font-semibold">
+                    <Link href={"/"}>STEM Events</Link>
+                </div>
+            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            </div>
+        </nav>
+    );
 } 
