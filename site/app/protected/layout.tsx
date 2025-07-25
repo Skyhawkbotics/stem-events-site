b6ee2a029932298b3585ba80a91f8e99cae0c36d
Dev Sahay <59460385+Synaptic149@users.noreply.github.com>
2025-07-24 22:34:19 -0700

    use common navbar and footer in protected page

--- site/app/protected/layout.tsx
+++ site/app/protected/layout.tsx
@@ -3,6 +3,8 @@ import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
+import { Navbar } from "@/components/navbar";
+import {Footer} from "@/components/footer";
 
 export default function ProtectedLayout({
   children,
@@ -12,34 +14,12 @@ export default function ProtectedLayout({
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>STEM Events</Link>
-              <div className="flex items-center gap-2">
-              </div>
-            </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
+        <Navbar />
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-        <p>
-            Maintained by{" "}
-            <a
-              href="https://www.hawkbot1cs.org"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Hawkbot1cs
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
+        <Footer />
       </div>
     </main>
   );
