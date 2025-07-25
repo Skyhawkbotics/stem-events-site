805a15069cbb6e261a32689c2729e57fd761d200
Synaptic <devkrishnasahay@gmail.com>
2025-07-24 20:40:53 -0700

    add events list to navbar

--- site/components/navbar.tsx
+++ site/components/navbar.tsx
@@ -9,6 +9,7 @@ export function Navbar() {
             <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>STEM Events</Link>
+                    <Link href={"/events"}>Events List</Link>
                 </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
             </div>
