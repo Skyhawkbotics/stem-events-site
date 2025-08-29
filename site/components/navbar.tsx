7fc732feda3fa4c88780a473326b139a3c6c0cc1
Synaptic <devkrishnasahay@gmail.com>
2025-08-29 08:37:17 -0700

    add logo to navbar

--- site/components/navbar.tsx
+++ site/components/navbar.tsx
@@ -2,13 +2,17 @@ import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
+import Image from "next/image";
 
 export function Navbar() {
     return (
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
             <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>FIRSTFinder.org</Link>
+                    <Link href={"/"} className="flex items-center gap-2">
+                        <Image src="/favicon.ico" alt="logo" width={20} height={20} />
+                        FIRSTFinder.org
+                    </Link>
                     <Link href={"/events"}>Events List</Link>
                 </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
