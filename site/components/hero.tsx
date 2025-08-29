49f6ce0cea1b669b570af7a54d55e9433eb44275
Synaptic <devkrishnasahay@gmail.com>
2025-08-29 15:58:47 -0700

    added gradient to hero component

--- site/components/hero.tsx
+++ site/components/hero.tsx
@@ -5,7 +5,15 @@ export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+      <p 
+        className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold"
+        style={{
+          background: 'linear-gradient(to right, #019bd6, #ee1c25)',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          backgroundClip: 'text'
+        }}
+      >
         Welcome to the new way to find STEM events in your area
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
