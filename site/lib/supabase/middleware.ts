738fcac93c5aaa69efb8658a9fb935190b8c824f
Ruben Yoder <rubenyoder@icloud.com>
2025-09-01 15:27:31 -0700

    Paths adjusted

--- site/lib/supabase/middleware.ts
+++ site/lib/supabase/middleware.ts
@@ -53,7 +53,9 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     request.nextUrl.pathname !== "/events" &&
-    !/^\/events\/[^/]+$/.test(request.nextUrl.pathname)
+    !/^\/events\/[^/]+$/.test(request.nextUrl.pathname) &&
+    request.nextUrl.pathname !== "/scrimmage" &&
+    !/^\/scrimmage\/[^/]+$/.test(request.nextUrl.pathname)
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
