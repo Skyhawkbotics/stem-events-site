47318a26769f08a00935693cf275fdf71dea65b7
Ruben Yoder <rubenyoder@icloud.com>
2025-09-01 14:58:27 -0700

    Another patch

--- site/app/scrimmage/[id]/page.tsx
+++ site/app/scrimmage/[id]/page.tsx
@@ -6,13 +6,13 @@ import { Footer } from "@/components/footer";
 export default async function EventPage({ params }: { params: { id: string } }) {
     const supabase = await createClient();
 
-    const { data: scrimmages, error } = await supabase
+    const { data: scrimmage, error } = await supabase
     .from("scrimmages")
     .select("*")
     .eq("id", params.id)
     .single();
 
-    if (error || !scrimmages) {
+    if (error || !scrimmage) {
         notFound(); // built-in Next.js 404
     }
 
@@ -20,11 +20,11 @@ export default async function EventPage({ params }: { params: { id: string } }) 
         <div>
             <Navbar />
             <div className="p-6 max-w-2xl mx-auto">
-                <h1 className="text-3xl font-bold mb-2">{scrimmages.title}</h1>
+                <h1 className="text-3xl font-bold mb-2">{scrimmage.title}</h1>
                 <p className="text-gray-600 text-sm mb-4">
-                    {new Date(scrimmages.scrimmage_date).toLocaleString()}
+                    {new Date(scrimmage.scrimmage_date).toLocaleString()}
                 </p>
-                <p>{scrimmages.scrimmage_description}</p>
+                <p>{scrimmage.scrimmage_description}</p>
             </div>
             <Footer />
         </div>
