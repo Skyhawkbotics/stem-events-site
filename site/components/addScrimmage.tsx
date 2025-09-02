06cce9ca0d410adb1709a201d8f24c70cb345ba6
Ruben Yoder <rubenyoder@icloud.com>
2025-09-02 09:31:59 -0700

    reloading page

--- site/components/addScrimmage.tsx
+++ site/components/addScrimmage.tsx
@@ -207,6 +207,7 @@ export default function AddScrimmage() {
                     className="flex-1 bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
                   >
                     {isLoading ? 'Adding...' : 'Add Scrimmage'}
+                    window.location.reload();
                   </button>
                 </div>
               </div>
