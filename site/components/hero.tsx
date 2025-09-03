d0596d46c36f934dcd42099a99269f5817958c2b
Synaptic <devkrishnasahay@gmail.com>
2025-09-02 17:56:52 -0700

    update the hero to include the logo

--- site/components/hero.tsx
+++ site/components/hero.tsx
@@ -1,12 +1,15 @@
-import { NextLogo } from "./next-logo";
-import { SupabaseLogo } from "./supabase-logo";
+import { BrandLogo } from "./brand-logo";
 
 export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <h1 className="sr-only">FIRST Tech Challenge Scrimmages and Events</h1>
+      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold">This is</p>
+      <div className="-mt-8">
+        <BrandLogo logoSize={48} textSize="text-4xl font-bold" />
+      </div>
       <p 
-        className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold"
+        className="text-2xl lg:text-3xl !leading-tight mx-auto max-w-xl text-center font-bold"
         style={{
           backgroundImage: 'linear-gradient(to right, var(--primary), var(--secondary))',
           WebkitBackgroundClip: 'text',
@@ -14,7 +17,7 @@ export function Hero() {
           backgroundClip: 'text'
         }}
       >
-        Welcome to the new way to find STEM events in your area
+        The all in one FTC robotics scrimmage and event coordinator.
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
