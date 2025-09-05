c634410b32d724a4a50472462edbcb8272005887
Synaptic <devkrishnasahay@gmail.com>
2025-09-05 09:15:07 -0700

    adjust hero text size

--- site/components/hero.tsx
+++ site/components/hero.tsx
@@ -4,12 +4,12 @@ export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <h1 className="sr-only">FIRST Tech Challenge Scrimmages and Events</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold">This is</p>
+      <p className="text-5xl lg:text-6xl !leading-tight mx-auto max-w-xl text-center font-bold">This is</p>
       <div className="-mt-8">
-        <BrandLogo logoSize={48} textSize="text-4xl font-bold" />
+        <BrandLogo logoSize={48} textSize="text-5xl font-bold" />
       </div>
       <p 
-        className="text-2xl lg:text-3xl !leading-tight mx-auto max-w-xl text-center font-bold"
+        className="text-2xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center font-bold"
         style={{
           backgroundImage: 'linear-gradient(to right, var(--primary), var(--secondary))',
           WebkitBackgroundClip: 'text',
