1ebc35e6b0965f96b333b348aef9a5be5ff54a23
Synaptic <devkrishnasahay@gmail.com>
2025-09-29 11:51:37 -0700

    improve the look of the view all scrimmages button

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           </div>
         </div>
         <Link href="/scrimmage"><button
-          className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+          className="bg-primary text-primary-foreground py-2 px-4 rounded-md hover:bg-primary/80 focus:outline-primary-foreground focus:ring-2 focus:ring-primary focus:ring-offset-2 transition-colors border border-primary-foreground"
         >
           View All Scrimmages
       </button></Link>
